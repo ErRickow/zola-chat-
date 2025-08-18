569681901a2e3588f0bb377e4db9e1f9de3134be
ErRickow <remixingmagelang@gmail.com>
2025-08-19 02:27:40 +0700

    fixed toolnameSearch

--- app/components/chat/message-assistant.tsx
+++ app/components/chat/message-assistant.tsx
@@ -64,23 +64,16 @@ export function MessageAssistant({
   const reasoningParts = parts?.find((part) => part.type === "reasoning")
   const contentNullOrEmpty = children === null || children === ""
   const isLastStreaming = status === "streaming" && isLast
-  const searchImageResults =
-    parts
+  const searchImageResults = parts
     ?.filter(
       (part) =>
       part.type === "tool-invocation" &&
       part.toolInvocation?.state === "result" &&
-      part.toolInvocation?.toolName === "imageSearch" &&
+      part.toolInvocation?.toolName === "imageSearchTool" &&
       part.toolInvocation?.result?.content?.[0]?.type === "images"
     )
-    .flatMap((part) =>
-      part.type === "tool-invocation" &&
-      part.toolInvocation?.state === "result" &&
-      part.toolInvocation?.toolName === "imageSearch" &&
-      part.toolInvocation?.result?.content?.[0]?.type === "images" ?
-      (part.toolInvocation?.result?.content?.[0]?.results ?? []) :
-      []
-    ) ?? []
+    .flatMap((part) => part.toolInvocation?.result?.content?.[0]?.results ?? []) ??
+    [];
   
   return (
     <Message
